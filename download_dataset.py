66db6cc61566cb2d9339e2dfcddd3a3963533e31
Nicholas Wood <horizon.nicholas14@gmail.com>
2022-02-25 23:26:29 -0500

    Updated download_dataset to delete invalid downloaded images and loaded images into directories

--- download_dataset.py
+++ download_dataset.py
@@ -1,4 +1,5 @@
 import csv, requests, os, shutil
+from PIL import Image
 
 def download_image(target_folder, image_name, image_link):
     img_data = requests.get(image_link).content
@@ -12,8 +13,18 @@ def download_image(target_folder, image_name, image_link):
 # Test array
 genres = ['Animation', 'Drama', 'Action_Adventure', 'Horror_Thriller', 'Comedy', 'Romance', 'Documentary']
 
+def verify_image(target_sub_directory, image_name):
+    try:
+        #print('./' + target_sub_directory + '/' + image_name)
+        img = Image.open('./' + target_sub_directory + '/' + image_name + '.jpg')
+        img.verify()
+    except OSError:
+        print("{} is bad".format(image_name))
+        return False
+    return True
+
 def download_dataset(target_folder, csv_name):
-    with open(csv_name + '.csv', 'r') as csvfile:
+    with open(csv_name + '.csv', 'r', encoding="utf-8") as csvfile:
         data = csv.reader(csvfile)
         rows = []
         
@@ -40,10 +51,43 @@ def download_dataset(target_folder, csv_name):
 
 
             target_sub_directory = target_folder + '/' + genre
-            image_name = name + '_' + year + '_' + genre + '_' + str(genre_counts[genre])
+            image_name = genre + '_' + str(genre_counts[genre])
             print("Download image {}".format(image_name))
             download_image(target_sub_directory, image_name, poster_link)
+            good_image = verify_image(target_sub_directory, image_name)
             genre_counts[genre] += 1
+            if not good_image:
+                os.remove(target_sub_directory + '/' + image_name + '.jpg')
+                print("Remove {}".format(image_name))
 
 # Test code to download images from sample csvs
-download_dataset('data/sample_test', 'Testing')
+#download_dataset('data/sample_test', 'Testing')
+
+
+def verify_dataset(csv_name):
+    genre_counts = {}
+    for genre in genres:
+        genre_counts[genre] = 0
+
+
+    with open(csv_name + '.csv', 'r', encoding="utf-8") as csvfile:
+        data = csv.reader(csvfile)
+        rows = []
+
+        for row in data:
+            rows.append(row)
+
+        for r in range (0, len(rows)):
+            row = rows[r]
+            genre = row[2]
+            image_name = genre + '_' + str(genre_counts[genre])
+            
+            print("Download image {} {}".format(r, image_name))
+            genre_counts[genre] += 1
+
+#verify_dataset('Training')
+
+
+#download_dataset('data/sample_test', 'Testing')
+#download_dataset('data/sample_train', 'Training')
+download_dataset('data/sample_validation', 'Verification')
